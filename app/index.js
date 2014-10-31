327be3b9fb6f542ef78f278f42895c700ce7ec96
Chris Parker <mrcsparker@gmail.com>
2014-10-31 14:13:24 -0500

    Moved some things around

---
+++ app/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var generators = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+var TriangularGenerator = generators.Base.extend({
+
+  init: function() {
+    this.pkg = require('../package.json');
+    this.argument('appname', { type: String, required: false });
+    this.appname = this.appname || path.basename(process.cwd());
+    this.appname = this._.camelize(this._.slugify(this._.humanize(this.appname)));
+  },
+
+  info: function () {
+    this.log(yosay(
+      chalk.red('Welcome!') + '\n' +
+      chalk.yellow('You are using the Triangular angular es6 generator!')
+    ));
+  },
+
+  method1: function () {
+    console.log('method 1 just ran');
+  },
+  method2: function () {
+    console.log('method 2 just ran');
+  }
+});
+
+module.exports = TriangularGenerator;
